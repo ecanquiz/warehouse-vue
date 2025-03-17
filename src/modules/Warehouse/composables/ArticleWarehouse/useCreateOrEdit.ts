e4eed0ef9d2a59f8b0dc504f7195b90a412c5643
Ernesto Canquiz <caribestic123@gmail.com>
2025-03-17 19:17:00 -0400

    feat: Consume endpoint to get items by category

--- src/modules/Warehouse/composables/ArticleWarehouse/useCreateOrEdit.ts
+++ src/modules/Warehouse/composables/ArticleWarehouse/useCreateOrEdit.ts
@@ -1,4 +1,4 @@
-import { onMounted, reactive, ref } from 'vue'
+import { onMounted, watch, ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { useStoreWarehouse } from "@/modules/Warehouse/stores/"
 import useHttp from "@/composables/useHttp"; //foreign_table_name
@@ -10,106 +10,27 @@ import type { Warehouse } from  "../../types/Warehouse"
 
 export default (article_warehouseId?: string) => {
   const storeWarehouse = useStoreWarehouse()
-  const router = useRouter();
+  const router = useRouter();  
+  const articles: any = ref([]);
   
-  const articles: any = ref([])
+  const {errors, pending} = useHttp()
   
-  const {  
-    errors,
-    pending,
-
-    getError
-  } = useHttp()
-  
-  onMounted(async () => {
-
+  const getWarehouseByUuid = async () => {
     pending.value = true
 
-    const x = await WarehouseService.getWarehouseByUuid(storeWarehouse.uuid)
-      .then((response) => {
-        return response.data
-      })
-      .catch((error) => {
-        console.error(error);        
-      })
-      pending.value = false
-      pending.value = true
-
-      console.log(x)
+    const {categories} = await WarehouseService.getWarehouseByUuid(storeWarehouse.uuid)
+      .then(response => response.data)
+      .catch(error => {console.error(error);})
     
-    ArticleService.getArticlesByCategories()
-      .then((response) => {
-        articles.value=response.data
-      })
-      .catch((error) => {
-        console.error(error);        
-      })
+    ArticleService.getArticlesByCategories(categories)
+      .then(response => {articles.value=response.data;})
+      .catch(error => {console.error(error);})
       pending.value = false
+  }
 
-    /*if (article_warehouseId) {
-      pending.value = true
-      ArticleWarehouseService.getArticleWarehouse(article_warehouseId)
-        .then((response) => { 
-          article_warehouse.article_id = response.data.data.article_id 
-          article_warehouse.warehouse_uuid = response.data.data.warehouse_uuid 
-          article_warehouse.stock_min = response.data.data.stock_min 
-          article_warehouse.stock_max = response.data.data.stock_max 
-          article_warehouse.status = response.data.data.status 
-          article_warehouse.id_user_insert = response.data.data.id_user_insert 
-          article_warehouse.id_user_update = response.data.data.id_user_update 
-        })
-        .catch((err) => {        
-          errors.value = getError(err)
-        })
-        .finally(() => {
-          pending.value = false;
-        })
-    }*/
-    
-    /*pending.value = true
-    WarehouseService.getHelpWarehouses()
-      .then((response) => {
-        warehouses.value = response.data
-       })
-       .catch((err) => {
-         errors.value = getError(err)
-       })
-       .finally(() => {
-         pending.value = false
-       })*/
-  })
+  onMounted(async () => await getWarehouseByUuid())
 
-  /*const insertArticleWarehouse = async (article_warehouse: ArticleWarehouse) => {  
-    pending.value = true
-    return ArticleWarehouseService.insertArticleWarehouse(article_warehouse)
-      .then((response) => {         
-        alert( response.data.message )
-        router.push( { path: '/article_warehouse' } )
-      })
-      .catch((err) => {                
-        console.log( err.response.data )
-        errors.value = getError(err)
-      })
-      .finally(() => {
-        pending.value = false
-      })
-  }*/
-
-  /*const updateArticleWarehouse = async (article_warehouse: ArticleWarehouse, article_warehouseId: string) => {
-    pending.value= true
-    return ArticleWarehouseService.updateArticleWarehouse(article_warehouseId, article_warehouse)
-      .then((response) => {
-        alert( response.data.message )
-        router.push( { path: '/article_warehouse' } )
-      })
-      .catch((err) => {                
-        console.log( err.response.data )
-        errors.value = getError(err)
-      })
-      .finally(() => {
-        pending.value = false
-      })
-  }*/
+  watch(() => storeWarehouse.uuid, () => getWarehouseByUuid())
   
   const submit = (article_warehouse: ArticleWarehouse, article_warehouseId?: string) => {  
     //!article_warehouseId ? insertArticleWarehouse(article_warehouse)  : updateArticleWarehouse(article_warehouse, article_warehouseId)
